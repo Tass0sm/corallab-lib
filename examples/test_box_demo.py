8a9e0166dd900b64c91c1f8d0b11823d0990f535
Tassos Manganaris <tassos.manganaris@gmail.com>
2023-11-16 14:34:43 -0500

    Add examples

---
+++ examples/test_box_demo.py
@@ -0,0 +1,70 @@
+"""Credit to: https://github.com/lyfkyle/pybullet_ompl/"""
+
+import os.path as osp
+import pybullet as p
+import math
+import sys
+sys.path.append("/usr/lib/python3/dist-packages")
+
+import pybullet_data
+from corallab_sim.using_pybullet.ompl import pb_ompl
+from corallab_sim.using_pybullet.robots.panda import Panda
+from corallab_sim.using_pybullet.robots.ur5_robotiq85 import UR5Robotiq85
+
+
+class BoxDemo():
+    def __init__(self):
+        self.obstacles = []
+
+        p.connect(p.GUI)
+        p.setGravity(0, 0, -9.8)
+        p.setTimeStep(1./240.)
+
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.loadURDF("plane.urdf")
+
+        # load robot
+        # self.robot = Panda([0, 0, 0], [0, 0, 0])
+        self.robot = UR5Robotiq85([0, 0, 0], [0, 0, 0])
+        self.robot.load()
+
+        # setup pb_ompl
+        self.pb_ompl_interface = pb_ompl.PbOMPL(self.robot, self.obstacles)
+        self.pb_ompl_interface.set_planner("BITstar")
+
+        # add obstacles
+        self.add_obstacles()
+
+    def clear_obstacles(self):
+        for obstacle in self.obstacles:
+            p.removeBody(obstacle)
+
+    def add_obstacles(self):
+        # add box
+        self.add_box([1, 0, 0.7], [0.5, 0.5, 0.05])
+
+        # store obstacles
+        self.pb_ompl_interface.set_obstacles(self.obstacles)
+
+    def add_box(self, box_pos, half_box_size):
+        colBoxId = p.createCollisionShape(p.GEOM_BOX, halfExtents=half_box_size)
+        box_id = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=colBoxId, basePosition=box_pos)
+
+        self.obstacles.append(box_id)
+        return box_id
+
+    def demo(self):
+        # start = [0, 0, 0, -1, 0, 1.5, 0]
+        # goal = [0, 1.5, 0, -0.1, 0, 0.2, 0]
+        start = [0, 0, 0, -1, 0, 1.5]
+        goal = [0, 1.5, 0, -0.1, 0, 0.2]
+
+        self.robot.set_state(start)
+        res, path = self.pb_ompl_interface.plan(goal)
+        if res:
+            self.pb_ompl_interface.execute(path)
+        return res, path
+
+if __name__ == '__main__':
+    env = BoxDemo()
+    env.demo()
