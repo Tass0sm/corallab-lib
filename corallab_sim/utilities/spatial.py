a8e66fc60aac6dcdf2d26a4fb45856f5b482c930
Tassos Manganaris <tassos.manganaris@gmail.com>
2024-02-08 14:36:23 -0500

    Modify spatial utilities to handle batch dimension

--- corallab_sim/utilities/spatial.py
+++ corallab_sim/utilities/spatial.py
@@ -14,25 +14,31 @@ def get_rotation(rotq=None, euler=None, rotvec=None, matrix=None, t_matrix=None)
     elif matrix is not None:
         m = R.from_matrix(matrix)
     elif t_matrix is not None:
-        m = R.from_matrix(t_matrix[:-1, :-1])
+        m = R.from_matrix(t_matrix[..., :-1, :-1])
 
     return m
 
 
-def get_transform(rotq=None, euler=None, rotvec=None, matrix=None, pos=(0, 0, 0)):
+def get_transform(rotq=None, euler=None, rotvec=None, matrix=None, pos=np.array([[0, 0, 0]])):
     """utility function to create transformation matrix from different input forms"""
-    trans = np.eye(4)
+    if pos.ndim == 1:
+        bs = 1
+    else:
+        bs = pos.shape[0]
+
+    trans = np.zeros((bs, 4, 4))
+    trans[:] = np.eye(4)
 
     if rotq is not None:
-        trans[:-1, :-1] = R.from_quat(rotq).as_matrix()
+        trans[:, :-1, :-1] = R.from_quat(rotq).as_matrix()
     elif euler is not None:
-        trans[:-1, :-1] = R.from_euler("xyz", euler).as_matrix()
+        trans[:, :-1, :-1] = R.from_euler("xyz", euler).as_matrix()
     elif rotvec is not None:
-        trans[:-1, :-1] = R.from_rotvec(rotvec).as_matrix()
+        trans[:, :-1, :-1] = R.from_rotvec(rotvec).as_matrix()
     elif matrix is not None:
-        trans[:-1, :-1] = matrix
+        trans[:, :-1, :-1] = matrix
 
-    trans[:-1, -1:] = np.array(pos).reshape(-1, 1)
+    trans[:, :-1, -1:] = pos.reshape(bs, -1, 1)
 
     return trans
 
@@ -53,6 +59,12 @@ def transform_point(transform, point):
     homogenous_point_prime = np.matmul(transform, homogenous_point)
     point_prime = homogenous_point_prime[0:3] / homogenous_point_prime[3]
     return point_prime
+
+def transform_points(transform, points):
+    homogenous_points = np.pad(points, ((0, 0), (0, 1)), mode="constant", constant_values=1).T
+    homogenous_points_prime = np.matmul(transform, homogenous_points).T
+    points_prime = homogenous_points_prime[:, :3] / homogenous_points_prime[:, 3:]
+    return points_prime
 
 
 def compute_transform(a_points, b_points):
@@ -82,12 +94,55 @@ def apply_transform(tr: tuple, src: np.ndarray) -> np.ndarray:
     return (Q @ Pinv @ (src - s_cen).T).T + d_cen
 
 
-def change_basis():
-    pass
+def change_basis(new_basis, old_basis_poses):
+    """convert position (meters given in the world frame) and rot (a scipy
+    spatial rotation) to a pose (x, y, z, rx, ry, rz) in the robot's base
+    frame
 
+    # I * [0, 0] = [1, 0. 1] * [?, ?]
+    #              [0, 1, 1]
+    #              [0, 0, 1]
+    # I * [0, 0] = [1, 0. 1] * [?, ?]
+    #              [0, 1, 1]
+    #              [0, 0, 1]
+    # I * [0, 0] = [1, 1] * [?, ?]
 
-def change_basis():
-    pass
+    For position:
+    T_w * p = T_b * p'
+    T_b^(-1) * T_w * p = p'
+    T_b^(-1) * I * p = p'
+    T_b^(-1) * p = p'
+
+    For rotation:
+    ...
+
+    """
+    new_basis = new_basis.cpu()
+
+    # add batch dim
+    if new_basis.ndim == 1:
+        new_basis = new_basis.unsqueeze(0)
+
+    T_b = get_transform(pos=new_basis[..., :3], rotq=new_basis[..., 3:]).squeeze()
+    T_b_inv = invert_transform(T_b)
+    R_b_inv = get_rotation(t_matrix=T_b_inv)
+
+    # add batch dim
+    if old_basis_poses.ndim == 1:
+        old_basis_poses = old_basis_poses.unsqueeze(0)
+
+    # change basis of points
+    old_positions = old_basis_poses[:, :3].cpu().numpy()
+    new_positions = transform_points(T_b_inv, old_positions)
+
+    # change basis of orn
+    old_rots = get_rotation(rotq=old_basis_poses[:, 3:].cpu().numpy())
+    new_rot = old_rots * R_b_inv
+    new_quats = new_rot.as_quat()
+
+    # combine
+    new_poses = np.hstack([new_positions, new_quats])
+    return new_poses
 
 
 # TODO: All planes
