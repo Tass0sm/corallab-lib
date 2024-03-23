f8c5d2a37439528adeeb319113fe1ab755953bb0
Tassos Manganaris <tassos.manganaris@gmail.com>
2024-03-23 12:52:54 -0400

    Add backend state/kwarg management

--- corallab_sim/backend_manager.py
+++ corallab_sim/backend_manager.py
@@ -3,32 +3,36 @@ import importlib
 
 
 class BackendManager:
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super().__new__(cls, *args, **kwargs)
-        return cls._instance
-
     def __init__(self):
         self.backend = None
+        self.backend_kwargs = {}
 
-    def set_backend(self, backend: str):
+    def set_backend(self, backend: str, **backend_kwargs):
         assert backend in backend_list
         self.backend = backend
+        self.backend_kwargs = backend_kwargs
 
     def get_backend(self, backend=None):
+        assert backend or self.backend is not None, "Need to select a backend!"
+
         try:
             return importlib.import_module(
                 "." + (backend or self.backend),
                 package="corallab_sim.backends"
             )
         except KeyError:
-            raise Exception("Need to select a backend!")
+            raise Exception("Backend not found!")
 
     def get_backend_attr(self, attr, backend=None):
         backend_module = self.get_backend(backend=backend)
         return getattr(backend_module, attr)
+
+    def get_backend_kwargs(self):
+        return self.backend_kwargs
+
+    def get_backend_kwarg(self, name):
+        return self.backend_kwargs[name]
+
 
 
 backend_manager = BackendManager()
