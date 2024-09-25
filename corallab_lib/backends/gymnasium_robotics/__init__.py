bd5fe57d771d95b652dcea65101e3758d7cd0350
Tassos Manganaris <tassos.manganaris@gmail.com>
2024-09-25 16:17:23 -0400

    Add gymnasium_robotics backend

---
+++ corallab_lib/backends/gymnasium_robotics/__init__.py
@@ -0,0 +1,1 @@
+from .gym_impl import GymnasiumRoboticsGym as GymImpl
---
+++ corallab_lib/backends/gymnasium_robotics/gym_impl.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+# from . import gyms
+
+import gymnasium as gym
+import gymnasium_robotics
+
+from ..gym_interface import GymInterface
+
+
+class GymnasiumRoboticsGym(GymInterface):
+
+    def __init__(
+            self,
+            id: str,
+            seed: int = 42,
+            **kwargs
+    ):
+        assert id == "FetchPickAndPlace-v3"
+
+        self.gym_impl = gym.make(id, render_mode="human")
+
+        self._obs, self._info = self.gym_impl.reset(seed=seed)
+
+    # @property
+    # def action_space(self):
+    #     return self.gym_impl.action_space
+
+    # def seed(self, x):
+    #     self.gym_impl.seed(x)
+
+    # def reset(self, **kwargs):
+    #     return self.gym_impl.reset(**kwargs)
+
+    # def step(self, action, **kwargs):
+    #     return self.gym_impl.step(action, **kwargs)
+
+    # def sample(self):
+    #     return self.gym_impl.sample()
+
+    # def render(self):
+    #     return self.gym_impl.render()
+
+    # def close(self):
+    #     self.gym_impl.close()
