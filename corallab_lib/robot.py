a3bc4f2695b540871c1db3a5cf5cfab1d7187cab
Tassos Manganaris <tassos.manganaris@gmail.com>
2024-10-03 17:17:03 -0400

    Fix mistake in Robot entity definition

--- corallab_lib/robot.py
+++ corallab_lib/robot.py
@@ -9,7 +9,7 @@ class Robot(Entity):
             from_impl=None,
             **kwargs
     ):
-        super().__init__("EnvImpl", *args, backend=backend, from_impl=from_impl, **kwargs)
+        super().__init__("RobotImpl", *args, backend=backend, from_impl=from_impl, **kwargs)
 
     def __eq__(self, other):
         return self.robot_impl.robot_id == other.robot_impl.robot_id
